8bb7a3d1052259f262ca95b57b46ecd5df2927d0
SapnaGulati <sapnagulati9990@gmail.com>
2021-06-14 17:55:23 +0530

    SourcesDataModel and FloatRatingView pod added

--- NewsApp/News Data Models/SourcesDataModel.swift
+++ NewsApp/News Data Models/SourcesDataModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+struct SourcesDataModel {
+    var status: String?
+    var sources: [Sources] = []
+    
+    init(detail:JSONDictionary) {
+        self.status = detail["status"] as? String
+        
+        for source in detail["sources"] as? JSONArray ?? []
+        {
+            let post = Sources(detail: source)
+            self.sources.append(post)
+        }
+    }
+}
+
+struct Sources {
+    var id: String?
+    var name: String?
+    var description: String?
+    var url: String?
+    var category: String?
+    var language: String?
+    var country: String?
+    
+    init(detail: JSONDictionary) {
+        self.id = detail["id"] as? String
+        self.name = detail["name"] as? String
+        self.description = detail["description"] as? String
+        self.url = detail["url"] as? String
+        self.category = detail["category"] as? String
+        self.language = detail["language"] as? String
+        self.country = detail["country"] as? String
+    }
+}
